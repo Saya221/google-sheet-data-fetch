3a835eb38e257a600288ca2464d678f18fa62a58
Lê Hoàng Nguyên <le.hoang.nguyen.bedev@gmail.com>
2023-06-05 17:24:38 +0700

    [Fix] Minor logic

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,6 @@
 
 class Answer < ApplicationRecord
   has_many :faqs_resourceable, as: :resourceable, dependent: :destroy
+
+  scope :by_ids, ->(ids) { where id: ids }
 end
--- app/models/faqs_resourceable.rb
+++ app/models/faqs_resourceable.rb
@@ -3,4 +3,6 @@
 class FaqsResourceable < ApplicationRecord
   belongs_to :faq
   belongs_to :resourceable, polymorphic: true
+
+  scope :by_resourceable_type, -> (type) { where resourceable_type: type}
 end
